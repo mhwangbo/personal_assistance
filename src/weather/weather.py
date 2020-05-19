224a6f58afef7949e5286f125297c82f6a129be9
Mi Hwangbo <mihwangbo@Mis-MacBook-Pro.local>
2020-05-19 23:05:51 +0900

    need to reformat codes

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -1,5 +1,6 @@
 from get_weather import GetWeather
 from save_weather_data import SaveWeatherData
+import pandas as pd
 
 class Weather:
     def __init__(self):
@@ -7,8 +8,6 @@ class Weather:
         self.vilageFcst_data = None
         self.ultraSrtNcst_data = None
 
-        self.DATE = 00000000
-        self.TIME = 0000
         self.TMN = 0
         self.TMX = 0
         self.REH = 0
@@ -20,14 +19,42 @@ class Weather:
         # current temp
         self.T1H = 0
 
-    def get_data(self):
-        gw = GetWeather()
-        
-        self.vilageFcst_data = gw.get_VilageFcst()
-        self.ultraSrtNcst_data = gw.get_UltraSrtNcst()
+        self.base_date = 00000000
+        self.base_time = 0000
 
-    def org_data(self):
+        self.gw = None
+        self.sw = SaveWeatherData()
+
+    def get_data(self):
+        if not self.same_date_time():
+            self.vilageFcst_data = self.gw.get_VilageFcst()
+            self.ultraSrtNcst_data = self.gw.get_UltraSrtNcst()
+        else:
+            pass
+            #self.vilageFcst_data, self.ultraSrtNcst_data = self.get_data_from_file()
+
+    # check if there it's base time and date is same as previous search
+    def same_date_time(self):
+        # UltraSrtNcst is used because it's term between base time is shorter
+        self.gw = GetWeather()
+        u_base_date, u_base_time = self.gw.get_base_time_UltraSrtNcst()
+        if u_base_date == self.base_date and u_base_time == self.base_time:
+            return True
+        else:
+            self.base_date = u_base_date
+            self.base_time = u_base_time
+        return False
+
+    # get weather data from file if base_date and base_time matches
+    def get_data_from_file(self):
         pass
+
+    def org_data(self, data: pd.DataFrame):
+        index = ['DATE', 'TIME', 'TMN', 'TMX', 'REH', 'POP', 'PTY', 'WSD', 'SKY']
+        ret = dict.fromkeys(set(index), 0)
+        ret['DATE'] = data[0]['fcstDate']
+        ret['TIME'] = data[0]['fcstTime']
+
 
 w = Weather()
 w.get_data()
